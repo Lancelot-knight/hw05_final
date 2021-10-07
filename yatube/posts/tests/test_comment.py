ed3cafd3e788900ae1bebb17a24518cc4cfa7dd6
Алексей <nightwolfero@yandex.ru>
2021-10-07 12:17:03 +0300

    Fix fixtures

--- yatube/posts/tests/test_comment.py
+++ yatube/posts/tests/test_comment.py
@@ -29,6 +29,13 @@ def post(user):
 
 
 @pytest.fixture
+def user(django_user_model):
+    return django_user_model.objects.create_user(
+        username='TestUser', password='123456789'
+    )
+
+
+@pytest.fixture
 def user_client(user, client):
     client.force_login(user)
     return client
